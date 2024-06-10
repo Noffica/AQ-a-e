d86d2d44c18f7fb6306aff5c869c732aff21c055
Nauman Mithani <nmithani1@gmail.com>
2024-06-10 16:49:50 -0700

    welcomePage's methods packaged.

--- page-objects/welcome-page.ts
+++ page-objects/welcome-page.ts
@@ -1,6 +1,6 @@
 import { Locator, Page, expect } from "@playwright/test";
-import { TransactionDetailsPage } from "./transaction-details-page";
 import { BasePage } from "./base-page";
+import { TransactionDetailsPage } from "./transaction-details-page";
 
 export class WelcomePage extends BasePage
 {
@@ -11,39 +11,46 @@ export class WelcomePage extends BasePage
     super(page);
 
     this.continueButton = page.getByRole("button", { name: "Continue" });
-    this.customerReferenceIDField = page.locator('input#customerReferenceId[name="customerReferenceId"]');
+    this.customerReferenceIDField = page.getByLabel('Enter Customer Reference ID');
   }
 
-  async goTo() {
+  async advanceToTransactionDetailsPage() {
+    this._goTo();
+    this._fillCustomerReferenceID();
+    this._clickContinue();
+    this._verifyHandOffToTransactionDetailsPage();
+  }
+
+  private async _goTo() {
     await this.page.goto(process.env.WELCOME_PAGE_URL);
   }
 
-  async fillCustomerReferenceID(customerReferenceID?: string) {
+  private async _fillCustomerReferenceID(customerReferenceID?: string) {
     const id = customerReferenceID || process.env.CUSTOMER_REFERENCE_ID;
     await this.customerReferenceIDField.fill(id);
   }
 
-  async clickContinue() {
+  private async _clickContinue() {
     await this.continueButton.click();
   }
 
-  async verifyHandOffToTransactionDetailsPage() {
+  private async _verifyHandOffToTransactionDetailsPage() {
     // Wait for the login API call to complete
     await this.page.waitForResponse(response =>
-      response.url() === 'https://checkout-dev.aquanow.io/api/login'
+      response.url().includes('/api/login')
       &&
       response.status() === 200
     );
     // Wait for the KYC API call to complete
     await this.page.waitForResponse(response =>
-      response.url() === 'https://checkout-dev.aquanow.io/rowan/3eb00b7e727b41218bc4964635fc2288/kyc?_rsc=efuaq'
+      response.url().includes('/kyc')
       &&
       response.status() === 200
     );
 
     const transactionDetailsPage = new TransactionDetailsPage(this.page);
 
-    // Confirm the 'Continue' button is disabled *and* that interaction with it has no effect
+    // Confirm the 'Continue' button is disabled
     expect(
       await transactionDetailsPage.continueButton.getAttribute('class')
     ).toContain('Mui-disabled');
--- tests/welcome.spec.ts
+++ tests/welcome.spec.ts
@@ -3,16 +3,19 @@ import { TransactionDetailsPage } from "../page-objects/transaction-details-page
 import { WelcomePage } from "../page-objects/welcome-page";
 
 test.describe("Welcome page", () => {
-  let browser: Browser;
-  let context: BrowserContext;
+  // let browser: Browser;
+  // let context: BrowserContext;
   let welcomePage: WelcomePage;
   let transactionDetailsPage: TransactionDetailsPage;
 
-  test.beforeAll(async({ browser }) => {
+  test.beforeAll(async({ browser, browserName }) => {
     const context = await browser.newContext();
+    if (browserName !== 'firefox') {
+      await context.grantPermissions(['clipboard-read']);
+    }
     const page = await context.newPage();
     welcomePage = new WelcomePage(page);
-    transactionDetailsPage = new TransactionDetailsPage(welcomePage.page);
+    transactionDetailsPage = new TransactionDetailsPage(page);
   });
 
   // TODO: remove if un-needed
@@ -21,11 +24,12 @@ test.describe("Welcome page", () => {
   // });
 
   test('go past "Welcome" page', async () => {
-    await welcomePage.goTo();
-    await welcomePage.fillCustomerReferenceID();
-    await welcomePage.clickContinue();
-    await welcomePage.verifyHandOffToTransactionDetailsPage();
+    await welcomePage.advanceToTransactionDetailsPage();
 
     await transactionDetailsPage.confirmSuccessfulPageLoad();
+  });
+
+  test('clipboard', async ({ browserName }) => {
+    test.skip(browserName === 'firefox', 'Clipboard permissions inaccessible in Firefox');
   });
 });
