6572b70fcdbd1668ffb30cd95e93278356b77f8a
Nauman Mithani <nmithani1@gmail.com>
2024-06-11 11:56:00 -0700

    Identifier vs. full Locator

--- page-objects/payment-instructions-page.ts
+++ page-objects/payment-instructions-page.ts
@@ -1,19 +1,22 @@
-import { Page, expect } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import { BasePage } from "./base-page";
 
 export class PaymentInstructionsPage extends BasePage
 {
   readonly page: Page;
-  readonly pageHeadingElement: [string, { name: string }] = ['heading', { name: 'Payment instructions' }];
+  // readonly pageHeadingIdentifier: [string, { name: string }] = ['heading', { name: 'Payment instructions' }];
+  readonly pageHeadingElement: Locator;
 
   constructor(page: Page) {
     super(page);
     this.page = page;
-    // this.pageHeadingElement = ['heading', { name: 'Payment instructions' }];
+    // this.pageHeadingIdentifier = ['heading', { name: 'Payment instructions' }];
+    this.pageHeadingElement = page.getByRole('heading', { name: 'Payment instructions' });
   }
 
   async confirmSuccessfulPageLoad() {
-    await expect(this.page.getByRole(...this.pageHeadingElement)).toBeVisible();
+    // await expect(this.page.getByRole(...this.pageHeadingIdentifier)).toBeVisible();
+    await expect(this.pageHeadingElement).toBeVisible();
   }
 } //end of class
 
